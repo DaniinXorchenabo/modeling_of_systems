a9fb8407d2f8092f84511e14627bb7c962d8095b
DaniinXorchenabo <rkbcu@mail.ru>
2022-05-16 21:22:00 +0300

    functional of 5th laboratory work is completed

--- lab_5/main.py
+++ lab_5/main.py
@@ -17,7 +17,6 @@ from wolframclient.language.expression import WLFunction, WLSymbol
 from wolframclient.utils.packedarray import PackedArray
 from wolframclient.exception import RequestException
 
-
 session: WolframCloudAsyncSession | None = None
 wolfram_code: str | None = None
 
@@ -57,7 +56,21 @@ async def create_connection_with_wolfram():
 
 
 @app.get('/calculate', response_model=list[list[Decimal]] | Any)
-async def test(n: int = 10, a: Decimal = 0, b: Decimal = 100, eps='10^-8'):
+async def test(
+        n: Decimal = 10,
+        a: Decimal = 0,
+        b: Decimal = 100,
+        eps='10^-8',
+        a2: Decimal = 3.5,
+        x_nodes: list[Decimal] = Query([-3.2, -0.8, 0.4, 2.8, 4, 6.4, 7.6]),
+        y_nodes: list[Decimal] = Query([-1.94, -0.61, 0.31, 1.81, 2.09, 1.47, 0.68]),
+        a3: Decimal = 0.8,
+        b3: Decimal = 0.95,
+        h03: Decimal = 0.05,
+        h3: Decimal = 0.025,
+        x_nodes3: list[Decimal] = Query([0.8, 0.85, 0.9, 0.95]),
+        y_nodes3: list[Decimal] = Query([0.717, 0.75, 0.783, 0.813])
+):
     """ ln(x) - (1 /(x + 1))  = 0 => x > 0 """
     global wolfram_code
     with open('code.nb', 'r', encoding='utf-8') as f:
@@ -69,7 +82,17 @@ async def test(n: int = 10, a: Decimal = 0, b: Decimal = 100, eps='10^-8'):
              f'b = {b};' \
              f'eps = {eps};' \
              'xData = {3, 5, 7};' \
-             'yData = {-3, -7, 1};'
+             'yData = {-3, -7, 1};' \
+             f'a2 = {a2};' \
+             f'xNodes = {"{" + ", ".join([str(i) for i in x_nodes]) + "}"};' \
+             f'yNodes = {"{" + ", ".join([str(i) for i in y_nodes]) + "}"};' \
+             f'a3 = {a3};' \
+             f'b3 = {b3};' \
+             f'h03 = {h03};' \
+             f'h3 = {h3};' \
+             f'xNodes3= {"{" + ", ".join([str(i) for i in x_nodes3]) + "}"};' \
+             f'yNodes3= {"{" + ", ".join([str(i) for i in y_nodes3]) + "}"};' \
+ \
     # print(params)
     try:
         data = session.evaluate(params + wolfram_code)
@@ -88,17 +111,34 @@ async def test(n: int = 10, a: Decimal = 0, b: Decimal = 100, eps='10^-8'):
     def normalized_result(awaited_data_):
         res_ = awaited_data_
         if isinstance(awaited_data_, (list, set, tuple, frozenset)):
-            res_ = [normalized_result(i) for i in awaited_data_]
+            if all([isinstance(i, WLFunction) and hasattr(i, "head") and hasattr(i.head,
+                                                                                 "name") and i.head.name == "Rule"
+                    for i in awaited_data_]):
+                res_ = dict()
+                [res_.update(normalized_result(i)) for i in awaited_data_]
+            else:
+                res_ = [normalized_result(i) for i in awaited_data_]
         if isinstance(awaited_data_, WLFunction):
-            res_ = [normalized_result(j) for j in awaited_data_.args]
-        if hasattr(awaited_data_, 'tolist'):
-            res_ = [list(map(lambda i: str(round(i, 30)), i)) for i in awaited_data_]
+            if hasattr(awaited_data_, "head") \
+                    and hasattr(awaited_data_.head, "name") and awaited_data_.head.name == "Rule":
+                res_ = {
+                    normalized_result(awaited_data_.args[0]): normalized_result(
+                        awaited_data_.args[1] if len(awaited_data_.args) == 2 else awaited_data_.args[1:]
+                    )
+                }
+            else:
+                res_ = [normalized_result(j) for j in awaited_data_.args]
+        # if hasattr(awaited_data_, 'tolist'):
+        #     res_ = [list(map(lambda i: str(round(i, 30)), i)) for i in awaited_data_]
+        if isinstance(res_, float) and -2 <= res_ <= 2:
+            res_ = float(round(Decimal(round(res_ * 100, 10)) / 100, 12))
         return res_
 
-    res: WLSymbol = normalized_result(awaited_data)
+    res: list | dict = normalized_result(awaited_data)
     print(awaited_data, sep='\n')
+    print(res)
 
-    return {i[0]: i[1] for i in res}
+    return res
 
 
 @app.get('/')
@@ -109,4 +149,4 @@ async def root_f():
 app.mount("/public", StaticFiles(directory=join(split(__file__)[0], 'public')), name="static")
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="localhost", port=9010, reload=True, reload_includes=['*.py', '*.nb'])
+    uvicorn.run("main:app", host="localhost", port=9015, reload=True, reload_includes=['*.py', '*.nb'])
